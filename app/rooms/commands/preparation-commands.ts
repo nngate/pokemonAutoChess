acb92b005b1e4a366c6814b748497758b40b05a2
nngate <uruwhy.geronimo@gmail.com>
2024-09-26 15:05:00 -0300

    min player message update

--- app/rooms/commands/preparation-commands.ts
+++ app/rooms/commands/preparation-commands.ts
@@ -161,7 +161,7 @@ export class OnGameStartRequestCommand extends Command<
       if (nbHumanPlayers < MIN_HUMAN_PLAYERS && process.env.MODE !== "dev") {
         this.state.addMessage({
           authorId: "Server",
-          payload: `Due to the current high traffic on the game, to limit the resources used server side, only games with a minimum of 8 players are authorized.`,
+          payload: `Only games with a minimum of ${MIN_HUMAN_PLAYERS} players are authorized.`,
           avatar: "0054/Surprised"
         })
         return
